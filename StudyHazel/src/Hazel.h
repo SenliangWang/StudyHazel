b872c8de1aab7b8b71f30ec64f96bd481ceaf435
wangsl <wsl961127@gmail.com>
2023-01-20 16:24:31 +0800

    setup basic Application and Entry Point

---
+++ StudyHazel/src/Hazel.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// use for application
+
+#include "Hazel/Application.h"
+
+
+// --- Entry Point -----
+
+#include "Hazel/EntryPoint.h"
---
+++ StudyHazel/src/Hazel/Application.cpp
@@ -0,0 +1,17 @@
+#include "Application.h"
+
+namespace Hazel {
+
+	Application::Application(){}
+
+	void Application::Run() {
+		while (1) {
+			std::cout << 1 << std::endl;
+		}
+	}
+
+	Application::~Application() {
+
+	}
+	
+}
